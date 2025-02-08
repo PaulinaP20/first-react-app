87bf0f17ec8bbfdc5ece079244dbb38fec405ac3
Paulina Partyka <paulinapartyka2@gmail.com>
2025-02-08 14:13:34 +0100

    add NavLink

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,6 @@
 import styles from './NavBar.module.scss'
+import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 
 const NavBar =() => {
@@ -9,9 +11,9 @@ const NavBar =() => {
                     <span className="fa fa-tasks"></span>
                 </a>
                 <ul className={styles.menu}>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/favorite">Favorite</a></li>
-                    <li><a href="/about">About</a></li>
+                    <li><NavLink className={({isActive}) => isActive ? styles.linkActive : undefined} to="/">Home</NavLink></li>
+                    <li><NavLink className={({isActive}) => isActive ? styles.linkActive : undefined} to="/favorite">Favorite</NavLink></li>
+                    <li><NavLink className={({isActive}) => isActive ? styles.linkActive : undefined}to="/about">About</NavLink></li>
                 </ul>
             </div>
         </nav>
