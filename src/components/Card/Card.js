08f4c635b44062bd8cfd256317946e53d56573a7
Paulina Partyka <paulinapartyka2@gmail.com>
2025-02-09 21:01:13 +0100

    add deleteCard

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,6 +2,7 @@ import styles from './Card.module.scss'
 import clsx from 'clsx';
 import { useDispatch } from 'react-redux';
 import { toggleCardFavorite } from '../../redux/cardsRedux';
+import { deleteCard } from '../../redux/cardsRedux';
 
 const Card= props => {
 
@@ -13,11 +14,17 @@ const Card= props => {
         dispatch(toggleCardFavorite(props.id));
     //console.log(props.isFavorite);
 
+    const handleDelete =() =>
+        dispatch(deleteCard(props.id));
+
     return (
         <li className={styles.card}>{props.title}
-            <i className={clsx("fa fa-star-o", props.isFavorite && styles.active)}
-            onClick={handleClick}
-            ></i>
+            <div>
+                <i className={clsx("fa fa-star-o", props.isFavorite && styles.active)}
+                onClick={handleClick}
+                ></i>
+                <i className="fa fa-trash" onClick={handleDelete} ></i>
+            </div>
         </li>
     );
 };
--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -13,9 +13,14 @@ export const toggleCardFavorite = payload =>({
   type:TOGGLE_CARD_FAVORITE, payload
 });
 
+export const deleteCard = payload => ({
+  type: DELETE_CARD, payload})
+
 const createActionName = actionName => `app/lists/${actionName}`;
+
 const ADD_CARD = createActionName('ADD_CARD');
 const TOGGLE_CARD_FAVORITE=createActionName('TOGGLE_CARD_FAVORITE')
+const DELETE_CARD=createActionName('DELETE_CARD')
 
 const cardsReducer = (statePart=[], action) => {
     switch (action.type) {
@@ -23,9 +28,10 @@ const cardsReducer = (statePart=[], action) => {
         return [...statePart, {...action.payload, id:shortid()}];
       case TOGGLE_CARD_FAVORITE:
         return statePart.map(card => (card.id===action.payload) ? {...card, isFavorite: !card.isFavorite} : card)
+      case DELETE_CARD:
+        return statePart.filter(card=> card.id!==action.payload)
         default:
           return statePart;
     }
   }
-
   export default cardsReducer;
